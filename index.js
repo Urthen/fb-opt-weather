fb6c51a6c3ea3e59e1f6f344d0ab9c3deefa47c2
Michael Pratt <urthen@gmail.com>
2015-04-09 13:02:08 -0500

    localization

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function getWeather(route, args) {
     var location = args.length ? querystring.escape(args.map(function (x) { return x.replace(/,/g, ''); })) : 'TX/Austin';
 
     // Set up the URL for the request
+    // Fun fact: Wunderground supports localization, but NOT for the conditions endpoint. Awesome.
     var url = 'http://api.wunderground.com/api/' + apiKey + '/conditions/q/' + location + '.json';
 
     // Make the request
@@ -21,20 +22,20 @@ function getWeather(route, args) {
 
         if (data) {
             // If we have weather data, return a formatted version
-            route.send(data.display_location.full + ': ' + data.weather + '; ' + data.temp_f + '° F (feels like ' + data.feelslike_f + '° F); winds ' + data.wind_string.substring(0, 1).toLowerCase() + data.wind_string.substring(1));
+            route.send('?weather_success_response', data.display_location.full, data.weather, data.temp_f, data.feelslike_f, data.wind_string.substring(0, 1).toLowerCase() + data.wind_string.substring(1));
         } else if (results.response.results) {
             // If Wunderground returns a list of cities, list them
             var possibilities = [];
             for (var index in results.response.results) {
                 possibilities.push('     ' + (parseInt(index) + 1) + '. ' + results.response.results[index].city + ', ' + results.response.results[index].state + ' ' + results.response.results[index].country);
             }
-            route.send('Wunderground found a few possibilities; try one of the following instead:\n' + possibilities.join('\n'));
+            route.send('?weather_suggestion_response', possibilities.join('\n'));
         } else if (results.response.error) {
             // If Wunderground returns an error, print it
-            route.send('Wunderground says: "' + results.response.error.description + '"');
+            route.send('?response_error', results.response.error.description);
         } else {
             // If we get this far, reply with a witty message
-            route.send("I don't know what you wanted, but you're not getting it (unknown error).");
+            route.send('?generic_error');
         }
 
     }.bind(this));
